42bd041d9cd7d5c6b1736c0a8144cc8f8697ba9e
Sung Kim <clay1013@gmail.com>
2022-10-02 23:13:00 -0700

    get rid of bottom content when refreshing

--- src/App.js
+++ src/App.js
@@ -24,54 +24,58 @@ function App() {
 
   return (
     <div className={(typeof data.location != "undefined")? ((data.current.is_day === 1) ? 'app change' : 'app') : 'app'}>
-      <div className="search">
-      <input
-        onChange={e => setLocation(e.target.value)}
-        value={location}
-        placeholder="Enter City"
-        onKeyPress={searchLocation}
-        type="text"
-      />
-      </div>
-
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            {data.location ? <h2>{data.location.name}, {data.location.region}</h2> : null}
-          </div>
-          <div className="time">
-            {data.location ? <p>{data.location.localtime}</p> : null}
-          </div>
-          <div className="temp">
-            {data.current ? <h1>{data.current.temp_f} °F</h1> : null}
-          </div>
-
-          <div className="side">
-            <div className="icon">
-              {data.current ? <img src={data.current.condition.icon} /> : null}
-            </div>
-            <div className="description">
-              {data.current ? <p>{data.current.condition.text}</p> : null}
-            </div>
-          </div>
+        <div className="search">
+          <input
+            onChange={e => setLocation(e.target.value)}
+            value={location}
+            placeholder="Enter City"
+            onKeyPress={searchLocation}
+            type="text"
+          />
         </div>
 
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">Celsius</p>
-            {data.current ? <p>{data.current.temp_c}°C</p> : null}
-          </div>
-          <div className="humidity">
-            <p className="bold">Humidity</p>
-            {data.current ? <p>{data.current.humidity}%</p> : null}
-          </div>
-          <div className="wind">
-            <p className="bold">Wind</p>
-            {data.current ? <p>{data.current.wind_mph} mph</p> : null}
-          </div>
+        <div className="container">
+            <div className="top">
+              <div className="location">
+                {data.location ? <h2>{data.location.name}, {data.location.region}</h2> : null}
+              </div>
+              <div className="time">
+                {data.location ? <p>{data.location.localtime}</p> : null}
+              </div>
+              <div className="temp">
+                {data.current ? <h1>{data.current.temp_f} °F</h1> : null}
+              </div>
+                <div className="side">
+                  <div className="icon">
+                    {data.current ? <img src={data.current.condition.icon} /> : null}
+                  </div>
+                  <div className="description">
+                    {data.current ? <p>{data.current.condition.text}</p> : null}
+                  </div>
+                </div>
+            </div>
+
+            {data.current != undefined &&
+              <div className="bottom">
+              <div className="celsius">
+                <p className="bold">Celsius</p>
+                {data.current ? <p>{data.current.temp_c}°C</p> : null}
+              </div>
+              <div className="humidity">
+                <p className="bold">Humidity</p>
+                {data.current ? <p>{data.current.humidity}%</p> : null}
+              </div>
+              <div className="wind">
+                <p className="bold">Wind</p>
+                {data.current ? <p>{data.current.wind_mph} mph</p> : null}
+              </div>
+            </div>
+            }
+              
+
         </div>
 
-      </div>
+
     </div>
   );
 }
